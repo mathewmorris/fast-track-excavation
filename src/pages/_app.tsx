20d3e65264c35f75e7e03cdf3091330432b76d9d
Mat Morris <10860014+mathewmorris@users.noreply.github.com>
2023-12-29 18:25:17 -0600

    intial setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,29 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import Link from "next/link";
+import { Roboto } from 'next/font/google';
 
 import "~/styles/globals.css";
 
+const roboto = Roboto({
+  weight: ['400', '700', '900'],
+  subsets: ['latin'],
+  variable: '--font-sans',
+})
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`${roboto.variable} font-sans bg-gray-100`}>
+      <nav className="flex p-4 justify-between items-center absolute w-full text-white font-bold z-10">
+        <Link href="/">
+          Logo
+        </Link>
+      </nav>
+      <Component {...pageProps} />
+      <footer className="p-8 bg-gray-900">
+      </footer>
+    </div>
+  )
 };
 
 export default MyApp;
+
