f58d4e2a9dc3b8ac3c5996e65b008e18292c4b89
Mat Morris <10860014+mathewmorris@users.noreply.github.com>
2023-12-30 05:15:38 -0600

    logo replacement

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import { Roboto } from 'next/font/google';
 
 import "~/styles/globals.css";
+import excavatorImage from '../../public/excavator.png';
+import Image from "next/image";
 
 const roboto = Roboto({
   weight: ['400', '700', '900'],
@@ -15,7 +17,13 @@ const MyApp: AppType = ({ Component, pageProps }) => {
     <div className={`${roboto.variable} font-sans bg-gray-100`}>
       <nav className="flex p-4 justify-between items-center absolute w-full text-white font-bold z-10">
         <Link href="/">
-          Logo
+          <div className="flex items-start">
+            <Image src={excavatorImage} alt="excavator" height={32} />
+            <div className="flex flex-col pl-2">
+              <p className="font-light leading-3 text-xl">FAST TRACK</p>
+              <p className="font-bold leading-6 text-xl">EXCAVATION</p>
+            </div>
+          </div>
         </Link>
       </nav>
       <Component {...pageProps} />
